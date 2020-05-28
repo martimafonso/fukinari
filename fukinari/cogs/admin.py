368269b234c629e892daacde375337fd84f93f68
martimafonso <49534332+martimafonso@users.noreply.github.com>
2020-05-28 21:43:01 +0100

    Update admin.py

--- fukinari/cogs/admin.py
+++ fukinari/cogs/admin.py
@@ -4,27 +4,8 @@ from discord.ext import commands
 
 class admin(commands.Cog):
 #Eventos
-	def __init__(self, client):
-		self.client = client
 
-	@commands.Cog.listener()
-	async def on_ready(self):
-		print(r"[Modulo admin carregado]")
-		print("\n\n")
-#Comandos
-	@commands.command()
-	async def kick(self, ctx, member : discord.Member, *, reason=None):
-		try:
-			await member.kick(reason=reason)
-		except discord.Forbidden as dsError:
-			embed = discord.Embed(
- 		        color = discord.Color.from_rgb(144, 12, 63)
-    			)
-			embed.add_field(name=f"Error {dsError}", value="Eu nao posso fazer isso (#-#)")
-			await ctx.send(embed=embed)
-			
-
-def setup(client):
+	def setup(client):
 	client.add_cog(admin(client))
 	
 	GUI/CLI/WEB dashboard
